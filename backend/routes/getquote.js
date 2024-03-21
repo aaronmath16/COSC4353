a69177c2b6ae9bf8711ab2be3fb7e730584e0b21
William Brieden <wabriede@CougarNet.UH.EDU>
2024-03-20 23:27:09 -0500

    Adding prices to quote page

--- backend/routes/getquote.js
+++ backend/routes/getquote.js
@@ -2,15 +2,20 @@ const express = require('express')
 const router = express.Router()
 const loggedIn = require('../passportauth').loggedIn
 const loggedOut = require('../passportauth').loggedOut
+
+var delivAddress = "614 Default Rd"
+var suggPrice = null
+var totPrice = null
+
 router.get('/',loggedIn,(req,res) =>{
-    res.render('quotePage.ejs', {delivAddress: "614 Default Rd"})
+    res.render('quotePage.ejs', {delivAddress: delivAddress, suggPrice: suggPrice, totPrice: totPrice})
 })
 router.post('/',loggedIn,(req,res) =>{
     //clientside validations go here Consult the register js route for an example
 })
 
 function calcPrice(req, res, next){
-    //Todo calculate suggested price
+    //Todo calculate suggested price and total price
     next()
 }
 
