fbc048c5c708839595eda31d52f2f085e61fa2f3
William Brieden <wabriede@CougarNet.UH.EDU>
2024-04-10 18:21:20 -0500

    Renaming database

--- backend/dbTest.js
+++ backend/dbTest.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Connect to the SQLite database
-const db = new sqlite3.Database('example.db', (err) => {
+const db = new sqlite3.Database('data.db', (err) => {
   if (err) {
     console.error('Error connecting to database:', err.message);
   } else {
--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use('/quotePage',quoteRouter)
 app.use('/logout',logoutRouter)
 
 // Connect to the SQLite database
-const db = new sqlite3.Database('example.db', (err) => {
+const db = new sqlite3.Database('data.db', (err) => {
     if (err) {
       console.error('Error connecting to database:', err.message);
     } else {
