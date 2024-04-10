5c8c23b06030fc430b7a46bb3bfa97720fbe27ca
Aaron Mathias <aaronmath16@gmail.com>
2024-04-10 18:51:51 -0500

    Setup QuoteHistory page to pull from db using sqlite

--- backend/routes/quotehistory.js
+++ backend/routes/quotehistory.js
@@ -4,6 +4,7 @@ const loggedIn = require('../passportauth').loggedIn
 const loggedOut = require('../passportauth').loggedOut
 const mockdb = require('../mockdb')
 const mockhistory = mockdb.history
+const sqlite3 = require('sqlite3').verbose()
 
 var tableEntries = mockhistory
 var tableHtml = ''
@@ -13,9 +14,31 @@ router.get('/',loggedIn,popHistory, (req,res) =>{
 
 })
 
-//This one might be a bit tricky especially without the DB put in yet.  Maybe a mock DB can be put in here alone and then use EJS to dynamically fill the tables with that info.
+const db = new sqlite3.Database('data.db', (err) => {
+    if (err) {
+      console.error('Error connecting to database:', err.message);
+    } else {
+      console.log('Connected to the database.');
+    }
+  });
 
-function popHistory(req, res, next) {
+tableHtml =''
+
+db.all(sql, [], (err, rows) => {
+    if (err) return console.error(err.message);
+
+    rows.forEach((row) => {
+        tableHtml += "<tr><td>" + entry.name + 
+                "</td><td>" + entry.gallonsRequested + 
+                "</td><td>" + entry.deliveryDate + 
+                "</td><td>" + entry.deliveryAddress + 
+                "</td><td> " + "$" + entry.suggestedPrice + 
+                "</td><td>" + "$" + entry.total + "</td></tr>";
+    });
+});
+
+
+/*function popHistory(req, res, next) {
     // Clear existing table rows
     tableHtml = ''
 
@@ -34,7 +57,16 @@ function popHistory(req, res, next) {
         res.redirect('quotePage.ejs',{error: "Error Fetching fuel history."})
     }
     next()
-}
+}*/
+
+
+db.close((err) => {
+    if (err) {
+      console.error('Error closing database:', err.message);
+    } else {
+      console.log('Database connection closed.');
+    }
+    });
 
 
 module.exports = router
