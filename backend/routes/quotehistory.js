57762634bf14a24b8cb61c759b9c9be5f83394ea
Aaron Mathias <aaronmath16@gmail.com>
2024-04-10 19:12:45 -0500

    Setup mockdb with new variables

--- backend/routes/quotehistory.js
+++ backend/routes/quotehistory.js
@@ -6,24 +6,25 @@ const mockdb = require('../mockdb')
 const mockhistory = mockdb.history
 const sqlite3 = require('sqlite3').verbose()
 
-/*var tableEntries = mockhistory*/
+var tableEntries = mockhistory
+var tableHtml =''
 
-/*router.get('/',loggedIn,popHistory, (req,res) =>{        
+router.get('/',loggedIn,popHistory, (req,res) =>{        
     res.render('fuelhistory.ejs', {table: tableHtml})
 
-})*/
+})
 
-const db = new sqlite3.Database('data.db', (err) => {
+/*const db = new sqlite3.Database('data.db', (err) => {
     if (err) {
       console.error('Error connecting to database:', err.message);
     } else {
       console.log('Connected to the database.');
     }
-  });
+  });*/
 
-var tableHtml =''
 
-db.all(sql, [], (err, rows) => {
+
+/*db.all(sql, [], (err, rows) => {
     if (err) return console.error(err.message);
 
     rows.forEach((row) => {
@@ -33,38 +34,38 @@ db.all(sql, [], (err, rows) => {
                 "</td><td> " + "$" + row.total_price + 
                 "</td><td>" + "$" + row.fee + "</td></tr>";
     });
-});
+});*/
 
 
-/*function popHistory(req, res, next) {
+function popHistory(req, res, next) {
     // Clear existing table rows
     tableHtml = ''
 
     // Populate table with fetched data
     try{
         tableEntries.forEach(entry =>{
-                tableHtml += "<tr><td>" + entry.name + 
-                "</td><td>" + entry.gallonsRequested + 
-                "</td><td>" + entry.deliveryDate + 
-                "</td><td>" + entry.deliveryAddress + 
-                "</td><td> " + "$" + entry.suggestedPrice + 
-                "</td><td>" + "$" + entry.total + "</td></tr>";
+                tableHtml +=  "</td><td>" + entry.gallons_requested  + 
+                "</td><td>" + entry.delivery_date + 
+                "</td><td>" + entry.address + 
+                "</td><td> " + "$" + entry.total_price  + 
+                "</td><td>" + "$" + entry.fee + "</td></tr>";
         })
     }catch(error) {
         console.error('Error fetching fuel history:', error);
         res.redirect('quotePage.ejs',{error: "Error Fetching fuel history."})
     }
     next()
-}*/
+}
 
 
-db.close((err) => {
+/*db.close((err) => {
     if (err) {
       console.error('Error closing database:', err.message);
     } else {
       console.log('Database connection closed.');
     }
     });
+    */
 
 
 module.exports = router
